386199d4c4631c63ed8d4ee876cd9020ca224bc0
aritzg <galdos.aritz@gmail.com>
2016-10-17 15:30:14 +0200

    created sample portlet

---
+++ docroot/WEB-INF/src/com/traintiumsa/sample1/Sample1Portlet.java
@@ -0,0 +1,11 @@
+package com.traintiumsa.sample1;
+
+import com.liferay.util.bridges.mvc.MVCPortlet;
+
+/**
+ * Portlet implementation class Sample1Portlet
+ */
+public class Sample1Portlet extends MVCPortlet {
+ 
+
+}
